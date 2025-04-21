76fc116f5f3798bf59793dfa59432f8f7500e48b
Lucas Valente <lucas.gx2@intlmkt.com.br>
2025-04-21 07:33:27 -0300

    feat: Update macOS Flutter configuration and dependencies
    - Added Podfile for macOS project setup.
    - Updated Flutter-Debug.xcconfig and Flutter-Release.xcconfig to include Pod support files.
    - Registered new plugins in GeneratedPluginRegistrant.swift for file_picker, flutter_gl_macos, and path_provider_foundation.
    - Updated pubspec.yaml to use the latest version of file_picker.
    - Updated pubspec.lock with new dependencies and versions.
    - Registered FlutterGlWindowsPlugin in generated_plugin_registrant.cc for Windows support.
    - Updated generated_plugins.cmake to include flutter_gl_windows in the plugin list.

--- frontend/macos/Flutter/GeneratedPluginRegistrant.swift
+++ frontend/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,12 @@
 import FlutterMacOS
 import Foundation
 
+import file_picker
+import flutter_gl_macos
+import path_provider_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
+  FlutterGlMacosPlugin.register(with: registry.registrar(forPlugin: "FlutterGlMacosPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
 }
